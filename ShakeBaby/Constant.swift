f1b5dc92d60bb29a874f0713b224733ab1c88122
Steven Chou <stevennote1130@gmail.com>
2017-06-10 13:17:50 +0800

    Add Constant file

---
+++ ShakeBaby/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  ShakeBaby
+//
+//  Created by steven.chou on 2017/6/10.
+//  Copyright © 2017年 stevenchou. All rights reserved.
+//
+
+import Foundation
+
+struct Constant {
+
+    struct SampleConstant {
+
+        static let PROPOTY = "PROPOTY"
+        
+    }
+    
+}
