fee4809a55db03261ca9ba05eec73564fbdfad6f
Steven Chou <stevennote1130@gmail.com>
2017-06-10 10:19:19 +0800

    Deleted ViewController and added HomeViewController

---
+++ ShakeBaby/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  ShakeBaby
+//
+//  Created by steven.chou on 2017/6/10.
+//  Copyright © 2017年 stevenchou. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
