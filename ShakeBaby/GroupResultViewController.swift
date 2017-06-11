62f4b9f6d4ab15907f40a4217ceb96f04cc59ed9
謝豐任 <volvol@xiefengrende-MacBook-Pro.local>
2017-06-11 13:47:02 +0800

    getresult

--- ShakeBaby/GroupResultViewController.swift
+++ ShakeBaby/GroupResultViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class GroupResultViewController: UIViewController {
-
+    
+    //var resultArray = []
     override func viewDidLoad() {
         super.viewDidLoad()
         getResult()
@@ -31,8 +32,7 @@ class GroupResultViewController: UIViewController {
             
             guard let data = data else { return }
             guard let stringData = String(data: data, encoding: .utf8) else { return }
-            print(stringData)
-            
+            let resultArray = stringData.components(separatedBy: ",")
             
             
         }
@@ -40,4 +40,31 @@ class GroupResultViewController: UIViewController {
         task.resume()
     }
 
+}
+
+extension Dictionary {
+    func sortedKeys(isOrderedBefore:(Key,Key) -> Bool) -> [Key] {
+        return Array(self.keys).sorted(by: isOrderedBefore)
+    }
+    
+    // Slower because of a lot of lookups, but probably takes less memory (this is equivalent to Pascals answer in an generic extension)
+    func sortedKeysByValue(isOrderedBefore:(Value, Value) -> Bool) -> [Key] {
+        return sortedKeys {
+            isOrderedBefore(self[$0]!, self[$1]!)
+        }
+    }
+    
+    // Faster because of no lookups, may take more memory because of duplicating contents
+    func keysSortedByValue(isOrderedBefore:(Value, Value) -> Bool) -> [Key] {
+        return Array(self)
+            .sorted() {
+                let (_, lv) = $0
+                let (_, rv) = $1
+                return isOrderedBefore(lv, rv)
+            }
+            .map {
+                let (k, _) = $0
+                return k
+        }
+    }
 }
